ae5e638f5c2c6b18fae2691441be593491e8ba5f
DipendraPatel12 <dipendrapatel926@gmail.com>
2025-02-27 16:02:33 +0530

    images add on cloudinary for faster loading

--- Frontend/src/components/Loader.jsx
+++ Frontend/src/components/Loader.jsx
@@ -1,7 +1,7 @@
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center absolute left-1/2 top-32 transform -translate-x-1/2">
-      <div className="w-16 h-16 border-4 border-accent border-t-transparent rounded-full animate-spin"></div>
+    <div className="flex justify-center items-center absolute left-1/2 top-64 transform -translate-x-1/2">
+      <div className="w-10 h-10 border-4 border-accent border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
 };
