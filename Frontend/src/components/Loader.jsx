d431dccf3a3cec65e1f2978b2ad6a6b8a9a685ea
DipendraPatel12 <dipendrapatel926@gmail.com>
2025-02-27 16:12:09 +0530

    Loader component added

--- Frontend/src/components/Loader.jsx
+++ Frontend/src/components/Loader.jsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center absolute left-1/2 sm:top-80 top-64 transform -translate-x-1/2">
+    <div className="flex justify-center items-center absolute left-1/2  top-80 transform -translate-x-1/2">
       <div className="w-10 h-10 border-4 border-accent border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
